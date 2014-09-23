3280b4b831985a79426b8725d59097c9a7b60526
Nathan Dunn <ndunn@me.com>
2014-09-22 21:47:40 -0700

    removed a bad import (junit.*.Test) in non-test . .  IDE *fixed* imports, updated .gitignore

--- src/main/java/org/monarchinitiative/owlsim/eval/ProfileMatchEvaluator.java
+++ src/main/java/org/monarchinitiative/owlsim/eval/ProfileMatchEvaluator.java
@@ -1,29 +1,23 @@
 package org.monarchinitiative.owlsim.eval;
 
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
+import com.googlecode.javaewah.EWAHCompressedBitmap;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.monarchinitiative.owlsim.compute.matcher.ProfileMatcher;
 import org.monarchinitiative.owlsim.io.JSONWriter;
 import org.monarchinitiative.owlsim.kb.BMKnowledgeBase;
 import org.monarchinitiative.owlsim.kb.LabelMapper;
 import org.monarchinitiative.owlsim.kb.NonUniqueLabelException;
-import org.monarchinitiative.owlsim.kb.ewah.EWAHUtils;
 import org.monarchinitiative.owlsim.kb.filter.UnknownFilterException;
-import org.monarchinitiative.owlsim.model.match.ProfileQuery;
-import org.monarchinitiative.owlsim.model.match.Match;
-import org.monarchinitiative.owlsim.model.match.MatchSet;
-import org.monarchinitiative.owlsim.model.match.ProfileQueryFactory;
-import org.monarchinitiative.owlsim.model.match.QueryWithNegation;
+import org.monarchinitiative.owlsim.model.match.*;
 import org.monarchinitiative.owlsim.model.match.impl.ProfileQueryImpl;
 import org.monarchinitiative.owlsim.model.match.impl.QueryWithNegationImpl;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
-import com.googlecode.javaewah.EWAHCompressedBitmap;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Set;
+
+//import org.junit.Assert;
 
 /**
  * For evaluating test queries against a knowledge base using a profileMatcher
