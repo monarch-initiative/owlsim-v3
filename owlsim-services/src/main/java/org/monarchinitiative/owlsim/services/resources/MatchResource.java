d4b241721a8f802213b59ca6def2e6874b463a60
Christopher Condit <condit@sdsc.edu>
2014-10-31 11:45:17 -0700

    Uses new exceptions

--- owlsim-services/src/main/java/org/monarchinitiative/owlsim/services/resources/MatchResource.java
+++ owlsim-services/src/main/java/org/monarchinitiative/owlsim/services/resources/MatchResource.java
@@ -15,8 +15,6 @@
  */
 package org.monarchinitiative.owlsim.services.resources;
 
-import static com.google.common.collect.Sets.newHashSet;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -27,17 +25,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response.Status;
 
+import org.monarchinitiative.owlsim.compute.matcher.NegationAwareProfileMatcher;
 import org.monarchinitiative.owlsim.compute.matcher.ProfileMatcher;
 import org.monarchinitiative.owlsim.model.match.MatchSet;
 import org.monarchinitiative.owlsim.model.match.ProfileQuery;
 import org.monarchinitiative.owlsim.model.match.ProfileQueryFactory;
+import org.monarchinitiative.owlsim.services.exceptions.NonNegatedMatcherException;
+import org.monarchinitiative.owlsim.services.exceptions.UnknownMatcherException;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 
 @Path("/match")
 @Api(value = "/match", description = "match services")
@@ -49,24 +49,33 @@ public class MatchResource {
 
 	@GET
 	@Path("/matchers")
-	@ApiOperation(value = "Get registered profile matchers", response = Collection.class)
+	@ApiOperation(value = "Get registered profile matchers", response = Collection.class,
+	notes= "Additional notes on the matchers resource.")
 	public Collection<String> getMatchers() {
 		return matchers.keySet();
 	}
 
 	@GET
 	@Path("/{matcher}")
-	@ApiOperation(value = "Match", response = MatchSet.class)
+	@ApiOperation(value = "Match", response = MatchSet.class, 
+	notes= "Additional notes on the match resource.")
 	public MatchSet getMatches(
+			@ApiParam( value = "The name of the matcher to use", required = true)
 			@PathParam("matcher") String matcherName,
-			@QueryParam("id") Set<String> ids) {
+			@ApiParam( value = "IDs that should match", required = false)
+			@QueryParam("id") Set<String> ids,
+			@ApiParam( value = "IDs that should not match", required = false)
+			@QueryParam("negatedId") Set<String> negatedIds) {
 		if (!matchers.containsKey(matcherName)) {
-			throw new WebApplicationException(Status.NOT_FOUND);
+			throw new UnknownMatcherException(matcherName);
 		}
 		ProfileMatcher matcher = matchers.get(matcherName);
-		ProfileQuery query = ProfileQueryFactory.createQuery(newHashSet("http://x.org/cephalopod"));
+		// Verify that matcher is negation aware if negated IDs are used
+		if (!negatedIds.isEmpty() && !NegationAwareProfileMatcher.class.isAssignableFrom(matcher.getClass())) {
+			throw new NonNegatedMatcherException(matcherName);
+		}
+		ProfileQuery query = ProfileQueryFactory.createQueryWithNegation(ids, negatedIds);
 		return matcher.findMatchProfile(query);
 	}
-
 
 }
