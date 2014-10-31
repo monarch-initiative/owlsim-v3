c4d28ec59f17cee428733a6a21e504cedaa36917
Christopher Condit <condit@sdsc.edu>
2014-10-31 13:44:34 -0700

    Adds timing and cache to match resource

--- owlsim-services/src/main/java/org/monarchinitiative/owlsim/services/resources/MatchResource.java
+++ owlsim-services/src/main/java/org/monarchinitiative/owlsim/services/resources/MatchResource.java
@@ -15,9 +15,12 @@
  */
 package org.monarchinitiative.owlsim.services.resources;
 
+import io.dropwizard.jersey.caching.CacheControl;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -35,6 +38,7 @@ import org.monarchinitiative.owlsim.model.match.ProfileQueryFactory;
 import org.monarchinitiative.owlsim.services.exceptions.NonNegatedMatcherException;
 import org.monarchinitiative.owlsim.services.exceptions.UnknownMatcherException;
 
+import com.codahale.metrics.annotation.Timed;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
@@ -57,6 +61,8 @@ public class MatchResource {
 
 	@GET
 	@Path("/{matcher}")
+	@Timed
+	@CacheControl(maxAge = 2, maxAgeUnit = TimeUnit.HOURS)
 	@ApiOperation(value = "Match", response = MatchSet.class, 
 	notes= "Additional notes on the match resource.")
 	public MatchSet getMatches(
