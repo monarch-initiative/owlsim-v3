3de93c1773deb79ebc888ba635673bd02d306c44
Christopher Condit <condit@sdsc.edu>
2014-10-31 10:51:53 -0700

    Adds negated profile query with IDs to factory

--- owlsim-core/src/main/java/org/monarchinitiative/owlsim/model/match/ProfileQueryFactory.java
+++ owlsim-core/src/main/java/org/monarchinitiative/owlsim/model/match/ProfileQueryFactory.java
@@ -8,16 +8,20 @@ import org.monarchinitiative.owlsim.model.match.impl.ProfileQueryImpl;
 import org.monarchinitiative.owlsim.model.match.impl.QueryWithNegationImpl;
 
 public class ProfileQueryFactory {
-	
+
 	public static ProfileQuery createQuery(Set<String> labels, LabelMapper labelMapper) throws NonUniqueLabelException {
 		Set<String> qids = labelMapper.lookupByUniqueLabels(labels);
 		ProfileQueryImpl q = new ProfileQueryImpl(qids);
 		return q;
 	}
-	
+
 	public static ProfileQuery createQuery(Set<String> ids) {
 		ProfileQueryImpl q = new ProfileQueryImpl(ids);
 		return q;
+	}
+
+	public static ProfileQuery createQueryWithNegation(Set<String> ids, Set<String> negatedIds) {
+		return QueryWithNegationImpl.create(ids, negatedIds);
 	}
 
 	public static ProfileQuery createQueryWithNegation(Set<String> labels, Set<String> negatedLabels, LabelMapper labelMapper) throws NonUniqueLabelException {
@@ -26,7 +30,5 @@ public class ProfileQueryFactory {
 		QueryWithNegation q = QueryWithNegationImpl.create(qids, nqids);
 		return q;
 	}
-
-	
 
 }
